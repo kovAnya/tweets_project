314663117333835ae6c82da5b439b4924f6369ef
Anna Kovalchuk <annazukh@gmail.com>
2023-04-19 01:07:27 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="tweets_project">
+    <BrowserRouter basename="/tweets_project/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
