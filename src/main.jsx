9969b5544f2d14f2afcea4195648b00d86500d61
Anna Kovalchuk <annazukh@gmail.com>
2023-04-19 12:35:12 +0300

    change_settings

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="tweets/">
+    <BrowserRouter basename="tweets-repo/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
